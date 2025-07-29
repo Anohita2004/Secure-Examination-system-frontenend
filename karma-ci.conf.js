62e2ccfb29b660c6a431731e3dac534452911e93
Anohita <anohitamukherjee@gmail.com>
2025-07-29 12:05:07 +0530

    error resolved 4

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -2,8 +2,27 @@ module.exports = function (config) {
 	"use strict";
 
 	require("./karma.conf")(config);
+
 	config.set({
-		browsers: ["ChromeHeadless"],
-		singleRun: true
+		browsers: ["ChromeHeadlessCI"],
+		singleRun: true,
+
+		// ⏱ Timeout fixes
+		browserNoActivityTimeout: 60000,       // Wait 60s before timing out
+		browserDisconnectTimeout: 10000,       // Wait 10s before disconnect
+		browserDisconnectTolerance: 2,         // Retry up to 2 times
+
+		// 🧪 Custom launcher for CI
+		customLaunchers: {
+			ChromeHeadlessCI: {
+				base: "ChromeHeadless",
+				flags: [
+					"--no-sandbox",
+					"--disable-gpu",
+					"--disable-dev-shm-usage",
+					"--disable-software-rasterizer"
+				]
+			}
+		}
 	});
 };
