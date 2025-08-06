a67b4c9cfac37b28944770c16d5861319457609e
Anohita <anohitamukherjee@gmail.com>
2025-08-06 17:04:46 +0530

    modifying files

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,42 @@
-// karma-ui5 usage: https://github.com/SAP/karma-ui5
+// karma.conf.js
 module.exports = function (config) {
-	"use strict";
+  "use strict";
 
-	config.set({
-		frameworks: ["ui5"],
-		browsers: ["Chrome"]
-	});
+  config.set({
+    frameworks: ["ui5"],
+
+    ui5: {
+      type: "application",
+      paths: {
+        webapp: "webapp" // adjust if your source folder is different
+      }
+    },
+
+    files: [
+      { pattern: "webapp/test/unit/unitTests.qunit.js", included: true },
+      { pattern: "webapp/test/unit/**/*.js", included: false, served: true }
+    ],
+
+    browsers: ["ChromeHeadless"],
+
+    // Timeouts (optional but helpful in CI)
+    browserNoActivityTimeout: 120000,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 2,
+    captureTimeout: 120000,
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          "--disable-gpu",
+          "--disable-dev-shm-usage",
+          "--disable-software-rasterizer"
+        ]
+      }
+    },
+
+    singleRun: true
+  });
 };
