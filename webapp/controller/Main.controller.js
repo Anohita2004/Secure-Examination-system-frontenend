457c597076fb92dd88befb2ea515c10c03d88a84
Anohita <anohitamukherjee@gmail.com>
2025-08-06 12:27:40 +0530

    login screen: UI enhancement

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,46 +8,65 @@ sap.ui.define([
   return BaseController.extend("exam.controller.Main", {
     onInit: function () {
       this._avatars = [
-        
-  {
-    role: "Admin",
-    avatar: "images/admin.jpg",
-    route: "login-admin"
-  },
-  {
-    role: "Employee",
-    avatar: "images/employee.jpg",
-    route: "login-employee"
-  }
-];
-
-      
+        {
+          role: "Admin",
+          avatar: "images/admin.jpg",
+          route: "login-admin",
+          description: "Manage exams, users, and system settings"
+        },
+        {
+          role: "Employee",
+          avatar: "images/employee.jpg",
+          route: "login-employee",
+          description: "Take exams and view your results"
+        }
+      ];
 
       this._currentIndex = 0;
       this.getView().setModel(new JSONModel({
-        selected: this._avatars[this._currentIndex]
+        selected: this._avatars[this._currentIndex],
+        currentIndex: this._currentIndex,
+        totalRoles: this._avatars.length
       }));
     },
 
     onNextAvatar: function () {
       this._currentIndex = (this._currentIndex + 1) % this._avatars.length;
       this._updateAvatar();
+      this._updateRoleIndicators();
     },
 
     onPreviousAvatar: function () {
       this._currentIndex =
         (this._currentIndex - 1 + this._avatars.length) % this._avatars.length;
       this._updateAvatar();
+      this._updateRoleIndicators();
     },
 
     _updateAvatar: function () {
       const oModel = this.getView().getModel();
       oModel.setProperty("/selected", this._avatars[this._currentIndex]);
+      oModel.setProperty("/currentIndex", this._currentIndex);
+    },
+
+    _updateRoleIndicators: function () {
+      // This will be handled by the view binding
+      const oModel = this.getView().getModel();
+      oModel.setProperty("/currentIndex", this._currentIndex);
     },
 
     onLoginContinue: function () {
       const selectedRole = this._avatars[this._currentIndex];
-      this.getRouter().navTo(selectedRole.route);
+      
+      // Add a small delay for smooth transition
+      setTimeout(() => {
+        this.getRouter().navTo(selectedRole.route);
+      }, 200);
+    },
+
+    onAvatarClick: function () {
+      // Optional: Add click functionality to avatar
+      this.onNextAvatar();
     }
   });
 });
