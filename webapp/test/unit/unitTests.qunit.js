0a8c828be54c0872b2bdc93292371c4feac826e7
Anohita <anohitamukherjee@gmail.com>
2025-08-06 17:43:55 +0530

    fixing issue

--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -2,7 +2,7 @@
 QUnit.config.autostart = false;
 
 sap.ui.require([
-  "./controller/App.qunit"
+  "exam/test/unit/controller/App.qunit"
 ], function () {
     console.log("All tests loaded. Starting QUnit...");
   QUnit.start(); // 🚀 Starts test run after loading all modules
